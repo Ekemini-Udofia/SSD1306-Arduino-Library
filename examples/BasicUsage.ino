2d784a1511909253d880a56754565639b0f325b7
Ekemini-Udofia <ekeminiabasiudofia@gmail.com>
2026-01-17 16:24:12 +0100

    set layout for basic usage example

---
+++ examples/BasicUsage.ino
@@ -0,0 +1,13 @@
+#include <SSD1306_lib.hpp>
+
+void setup() {
+
+  display_init();
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
